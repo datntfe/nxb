40b3d48374c2174485e4cd763670b25e8dc4194d
datnt-fe <datnt.fe@gmail.com>
2023-03-24 02:51:51 +0700

    done

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,62 @@
-$( document ).ready(function() {
-
-  // Get started!
-
+jQuery(function($) {
+  jQuery(document).ready(function(){  
+    $('#carousel-1').slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3.5,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2.8,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2.2,
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+    $('#carousel-2').slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3.5,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2.8,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2.2,
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  }); 
 });
